a409d68a30ac72a2817e3e58eeb2a65bb238e7b9
Durjoy-dcp <unknownccxv@gmail.com>
2022-09-13 02:25:45 +0600

    date added

--- js/app.js
+++ js/app.js
@@ -10,3 +10,22 @@ function bookTicket() {
     const e = document.getElementById('fromBus');
     console.log(e.options[e.selectedIndex].text);
 }
+function showDate() {
+    let dateString = new Date();
+    let actualDate = new Date(dateString);
+    let newDate;
+
+    for (var i = 1; i <= 4; i++) {
+        newDate = new Date(actualDate.getFullYear(), actualDate.getMonth(), actualDate.getDate() + i);
+        // console.log(newDate);
+        const option = document.createElement('option');
+        option.setAttribute('value', i - 1);
+        option.innerText = newDate.toDateString();
+        document.getElementById('date').appendChild(option);
+    }
+}
+const loc = window.location.href.split('/');
+if (loc[loc.length - 1] == 'booking.html') {
+    // console.log("here");
+    showDate();
+} 
