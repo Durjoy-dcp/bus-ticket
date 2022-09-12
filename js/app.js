31293ae54d25cf6f3003f6978a386ca6424b84e2
Durjoy-dcp <unknownccxv@gmail.com>
2022-09-13 00:21:20 +0600

    2nd page added

--- js/app.js
+++ js/app.js
@@ -2,5 +2,7 @@ function check() {
     const email = document.getElementById('exampleInputEmail1');
     const pass = document.getElementById('exampleInputPassword1');
     console.log("its working ");
+    window.location.href = 'booking.html';
+
 
 }
