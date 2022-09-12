abb794e4b2715e50d399188a2652c8ba3cb5719e
Durjoy-dcp <unknownccxv@gmail.com>
2022-09-12 23:46:01 +0600

    home page done

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function check() {
+    const email = document.getElementById('exampleInputEmail1');
+    const pass = document.getElementById('exampleInputPassword1');
+    console.log("its working ");
+
+}
