3a0b2cf650794c82323ec974c3656ad3f61b6daf
Durjoy-dcp <unknownccxv@gmail.com>
2022-09-13 06:02:55 +0600

    available seat working

--- js/app.js
+++ js/app.js
@@ -34,17 +34,9 @@ if (loc[loc.length - 1].split('.')[0] == 'booking') {
 
 
 
-function busSelected() {
+function busSelected(booked) {
     console.log("working");
-    for (let i = 1; i < 4; i++) {
 
-
-        const seats = document.getElementById('seat');
-        const option = document.createElement('option');
-        option.setAttribute('value', i);
-        option.innerText = i;
-        seats.appendChild(option);
-    }
     getData();
 }
 
@@ -59,12 +51,48 @@ function getData() {
     const time = timeSelect.options[timeSelect.selectedIndex].text;
     let busSelect = document.getElementById('bus');
     const bus = busSelect.options[busSelect.selectedIndex].text;
+    // let seatSelect = document.getElementById('seat');
+    // const seat = seatSelect.options[seatSelect.selectedIndex].text;
     console.log(from);
     console.log(to);
     console.log(date);
     console.log(time);
     console.log(bus);
 
+    // saveDataOnLocalStorage(from, to, date, time, bus, seat);
+    let tickets = getDataFromLocalstorage();
+    let seats = tickets.filter(x => x.fromBus == from && x.toBus == to && x.date == date && x.time == time && x.bus == bus).map(x => x.seat);
+    console.log(seats);
+    const seatsField = document.getElementById('seat');
+    seatsField.innerText = '';
+    for (let i = 1; i <= 40; i++) {
+
+        if (seats.includes(i + '')) {
+            // console.log('no');
+            continue;
+        } else {
+            const option = document.createElement('option');
+            option.setAttribute('value', i);
+            option.innerText = i;
+            seatsField.appendChild(option);
+        }
+
+
+    }
+
     // let datas = localStorage
 }
 document.getElementById('seat').addEventListener('focus', busSelected);
+function getDataFromLocalstorage() {
+    let ticketString = localStorage.getItem('tickets');
+    let tickets = [];
+    if (ticketString) {
+        tickets = JSON.parse(ticketString);
+    }
+    return tickets;
+}
+function saveDataOnLocalStorage(from, to, date, time, bus, seat) {
+    let seats = getDataFromLocalstorage();
+    seats.push({ fromBus: from, toBus: to, date: date, time: time, bus: bus, seat: seat });
+    localStorage.setItem('tickets', JSON.stringify(seats));
+}
